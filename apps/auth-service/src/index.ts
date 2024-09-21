3ea963d98b09bf0734b385b8c81b9a0fab5ed5bf
Harith Iqbal <hrithiqball@gmail.com>
2024-09-22 01:55:54 +0800

    build: added docker file for backend service

--- apps/auth-service/src/index.ts
+++ apps/auth-service/src/index.ts
@@ -1,5 +1,5 @@
-import fs from 'fs'
-import { createSecureServer } from 'http2'
+import { readFileSync } from 'node:fs'
+import { createSecureServer } from 'node:http2'
 import { env } from '@app/auth-service/config/env'
 import { authRouter } from '@app/auth-service/routes/auth'
 import { userRouter } from '@app/auth-service/routes/user'
@@ -7,11 +7,6 @@ import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import { logger } from 'hono/logger'
-
-const options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
-}
 
 export const app = new Hono()
   .basePath('/api')
@@ -25,7 +20,10 @@ console.log(`🔐 Auth service is running on port ${env.PORT}`)
 serve({
   fetch: app.fetch,
   createServer: createSecureServer,
-  serverOptions: options,
+  serverOptions: {
+    key: readFileSync('key.pem'),
+    cert: readFileSync('cert.pem')
+  },
   port: env.PORT
 })
 
